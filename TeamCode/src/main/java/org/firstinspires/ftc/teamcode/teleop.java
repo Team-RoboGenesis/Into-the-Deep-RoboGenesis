f956854612c8327672e078a32f629a533b450282
Team RoboGenesis <henryrdurso@icloud.com>
2024-11-02 12:51:35 -0700

    11/2/24

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop.java
@@ -9,15 +9,16 @@ import com.qualcomm.robotcore.util.Range;
 
 @TeleOp(name = "teleop")
 public class teleop extends OpMode {
+
     public DcMotor frontLeftWheel   = null;
     public DcMotor frontRightWheel  = null;
     public DcMotor backLeftWheel = null;
     public DcMotor backRightWheel = null;
     public Servo mainIntake = null;
-    public Servo rightPivot = null;
-    public Servo leftPivot = null;
-
-
+    public DcMotor rightPivot = null;
+    public DcMotor leftPivot = null;
+    public DcMotor intakeArm = null;
+    public Servo jointServo = null;
 
     @Override
     public void init() {
@@ -26,35 +27,42 @@ public class teleop extends OpMode {
         backLeftWheel = hardwareMap.get(DcMotor.class, "backLeft");
         backRightWheel = hardwareMap.get(DcMotor.class, "backRight");
         mainIntake = hardwareMap.get(Servo.class, "mainIntake");
-        rightPivot = hardwareMap.get(Servo.class,"rightPivot");
-        leftPivot = hardwareMap.get(Servo.class, "leftPivot");
-
+        rightPivot = hardwareMap.get(DcMotor.class,"rightPivot");
+        leftPivot = hardwareMap.get(DcMotor.class, "leftPivot");
+        intakeArm = hardwareMap.get(DcMotor.class,"intakeArm");
+        jointServo = hardwareMap.get(Servo.class, "joint");
         backRightWheel.setDirection(DcMotorSimple.Direction.REVERSE);
 
     }
-    public void setServoPos(double position) {
-        rightPivot.setPosition(position);
-        leftPivot.setPosition(position);
-
+    public void setServoPower(double power) { //it's kind of useless
+        rightPivot.setPower(power);
+        leftPivot.setPower(power);
     }
     @Override
     public void loop() {
         double y = -gamepad1.left_stick_y; // Remember, Y stick is reversed!
         double x = gamepad1.left_stick_x;
         double rx = gamepad1.right_stick_x;
+        double user2y = -gamepad2.left_stick_y;
+        double pivotUp = gamepad2.right_trigger;
+        double pivotDown = gamepad2.left_trigger;
+        double user2ry = gamepad2.right_stick_y;
 
         frontLeftWheel.setPower(y + x + rx);
         backLeftWheel.setPower(y - x + rx);
         frontRightWheel.setPower(y - x - rx);
         backRightWheel.setPower(y + x - rx);
-        if (gamepad1.b) {
-            mainIntake.setPosition(0.3);
-        } else if (gamepad1.x) {
+        intakeArm.setPower(user2y);
+        rightPivot.setPower(pivotDown);
+        leftPivot.setPower(pivotDown);
+        rightPivot.setPower(pivotUp);
+        leftPivot.setPower(pivotUp);
+        jointServo.setPosition(user2ry);
+
+        if (gamepad2.left_bumper) {
+            mainIntake.setPosition(0.2);
+        } else if (gamepad2.right_bumper) {
             mainIntake.setPosition(0.7);
-        } else if (gamepad1.a) {
-            setServoPos(0.5);
-        } else if (gamepad1.y) {
-            setServoPos(0.1);
         }
     }
 }
