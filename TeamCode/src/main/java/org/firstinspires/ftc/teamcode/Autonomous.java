b33d7f3ba052d2a1f9def5f3ecd6e529bee15040
Team RoboGenesis <henryrdurso@icloud.com>
2024-12-06 10:46:02 -0800

    re-adding teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
@@ -1,4 +1,26 @@
 package org.firstinspires.ftc.teamcode;
 
-public class Autonomous {
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@com.qualcomm.robotcore.eventloop.opmode.Autonomous(name = "Autonomous")
+public class Autonomous extends LinearOpMode {
+    /**
+     * @throws InterruptedException
+     */
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        Pose2d beginPose = new Pose2d(0, 0, Math.toRadians(90));
+        MecanumDrive drive = new MecanumDrive(hardwareMap, beginPose);
+        Action move = drive.actionBuilder(beginPose)
+                .turn(Math.toRadians(180))
+                //.lineToY(10)
+                .build();
+        waitForStart();
+
+        Actions.runBlocking(move);
+    }
 }
