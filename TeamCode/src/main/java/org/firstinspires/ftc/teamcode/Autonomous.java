e9b332df8117e81e8433f07a9c399b12ec9ac876
Team RoboGenesis <henryrdurso@icloud.com>
2024-12-07 20:47:04 -0800

    Auto v1 12/7/24

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
@@ -2,28 +2,88 @@ package org.firstinspires.ftc.teamcode;
 
 import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.TrajectoryBuilder;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @com.qualcomm.robotcore.eventloop.opmode.Autonomous(name = "Autonomous")
 public class Autonomous extends LinearOpMode {
+
+//    Servo mainIntake = hardwareMap.get(Servo.class, "mainIntake");
+//    Servo pivot = hardwareMap.get(Servo.class, "goBildaPivot");
+//    DcMotor rightIntakeArm = hardwareMap.get(DcMotor.class, "rightIntakeArm");
+//    DcMotor leftIntakeArm = hardwareMap.get(DcMotor.class, "leftIntakeArm");
+//    DcMotor slides = hardwareMap.get(DcMotor.class, "slides");
+
+//    public void setSlidePos(int position) {
+//        if (position>0) {
+//            slides.setTargetPosition(0);
+//        } else if (position<-1500) {
+//            slides.setTargetPosition(-1500);
+//        } else {
+//            slides.setTargetPosition(position);
+//        }
+//    }
+//    public void setArmPos(int position) {
+//        if (position < 0) {
+//            rightIntakeArm.setTargetPosition(0);
+//            leftIntakeArm.setTargetPosition(0);
+//        } else if (position > 2713) {
+//            rightIntakeArm.setTargetPosition(2713);
+//            leftIntakeArm.setTargetPosition(2713);
+//        }
+//        else {
+//            rightIntakeArm.setTargetPosition(position);
+//            leftIntakeArm.setTargetPosition(position);
+//        }
+//    }
+
     /**
      * @throws InterruptedException
      */
     @Override
     public void runOpMode() throws InterruptedException {
 
+//        rightIntakeArm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//        leftIntakeArm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//        rightIntakeArm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//        leftIntakeArm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//        rightIntakeArm.setTargetPosition(0);
+//        leftIntakeArm.setTargetPosition(0);
+//        rightIntakeArm.setPower(0.5);
+//        leftIntakeArm.setPower(0.5);
+//        rightIntakeArm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+//        leftIntakeArm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+//        slides.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//        slides.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//        slides.setTargetPosition(0);
+//        slides.setPower(0.75);
+//        slides.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        waitForStart();
+
+//        mainIntake.setPosition(0.1);
+//        wait();
+//        setArmPos(1000);
         Pose2d beginPose = new Pose2d(0, 0, Math.toRadians(90));
         MecanumDrive drive = new MecanumDrive(hardwareMap, beginPose);
         Action move = drive.actionBuilder(beginPose)
-                .lineToY(60)
+
+                .lineToY(30)
+                .lineToY(20)
                 .turn(Math.toRadians(90))
-                .lineToX(30)
-                .lineToX(-30)
-                .turn(Math.toRadians(180))
-//                .lineToY(100)
+                .lineToX(25)
+                .turn(Math.toRadians(45))
+                .lineToY(40)
+                .turn(Math.toRadians(-135))
+                .lineToY(5)
+                .lineToY(50)
+
                 .build();
-        waitForStart();
+
+  //      mainIntake.setPosition();
 
         Actions.runBlocking(move);
     }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentricDrive.java
@@ -0,0 +1,68 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+@TeleOp
+public class FieldCentricDrive extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        // Declare our motors
+        // Make sure your ID's match your configuration
+        DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeft");
+        DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeft");
+        DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRight");
+        DcMotor backRightMotor = hardwareMap.dcMotor.get("backRight");
+
+
+        frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        // Retrieve the IMU from the hardware map
+        IMU imu = hardwareMap.get(IMU.class, "imu");
+        // Adjust the orientation parameters to match your robot
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
+        // Without this, the REV Hub's orientation is assumed to be logo up / USB forward
+        imu.initialize(parameters);
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+            double y = gamepad1.left_stick_y; // Remember, Y stick value is reversed
+            double x = gamepad1.left_stick_x;
+            double rx = gamepad1.right_stick_x;
+
+            if (gamepad1.options) {
+                imu.resetYaw();
+            }
+
+            double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+
+            double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+            double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+
+            rotX = rotX * 1.1;
+
+            double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+            double frontLeftPower = (rotY + rotX + rx) / denominator;
+            double backLeftPower = (rotY - rotX + rx) / denominator;
+            double frontRightPower = (rotY - rotX - rx) / denominator;
+            double backRightPower = (rotY + rotX - rx) / denominator;
+
+            frontLeftMotor.setPower(frontLeftPower*1.4);
+            backLeftMotor.setPower(backLeftPower);
+            frontRightMotor.setPower(frontRightPower);
+            backRightMotor.setPower(backRightPower*1.4);
+        }
+    }
+}
