906c24e2e97bd59c08d9b51cec31a132e4921b3f
Team RoboGenesis <henryrdurso@icloud.com>
2024-12-29 21:53:19 -0800

    much better route

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FullRouteTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FullRouteTest.java
@@ -80,15 +80,24 @@ public class FullRouteTest extends LinearOpMode {
                 .waitSeconds(0.5)
                 .lineToY(-40)//line up for first sample push
                 .splineTo(new Vector2d(37, -38), Math.toRadians(90))
-                .lineToY(-17)
+                .splineTo(new Vector2d(37, -14), Math.toRadians(90))
                 .strafeTo(new Vector2d(46, -14))
                 .strafeTo(new Vector2d(46, -55))//move first sample into observation
-                .strafeTo(new Vector2d(46, -14))
-                .strafeTo(new Vector2d(55, -14))
-                .strafeTo(new Vector2d(55, -55))//move second sample into observation
-                .strafeTo(new Vector2d(55, -14))
-                .strafeTo(new Vector2d(62, -14))
-                .strafeTo(new Vector2d(62, -55))//move third sample into observation
+                .waitSeconds(0.3)
+                .strafeToLinearHeading(new Vector2d(7, -40), Math.toRadians(90)) //score second specimen
+                .strafeTo(new Vector2d(7, -30))
+                .waitSeconds(1)
+                .lineToY(-40)
+                .splineTo(new Vector2d(46, -38), Math.toRadians(90))
+                .splineTo(new Vector2d(46, -14), Math.toRadians(90))
+                .strafeTo(new Vector2d(57, -14))
+                .strafeTo(new Vector2d(57, -55))//move second sample into observation
+                .strafeTo(new Vector2d(47, -55))//score third specimen
+                .strafeToLinearHeading(new Vector2d(4, -40), Math.toRadians(90))
+                .strafeTo(new Vector2d(4, -30))
+                .waitSeconds(1)
+                .lineToY(-40)
+//                        .strafeToLinearHeading(new Vector2d(47, -50), Math.toRadians(-90))
                 .build();
         waitForStart();
 
