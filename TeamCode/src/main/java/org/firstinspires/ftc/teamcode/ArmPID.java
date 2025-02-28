add5327f077456e9bf9e7fd50aad71f67140a48b
oliver-error <oliverliamandersen2011@gmail.com>
2025-02-27 17:18:04 -0800

    arm PID P
    I wrote an unfinished P

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmPID.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmPID.java
@@ -1,4 +1,31 @@
 package org.firstinspires.ftc.teamcode;
 
-public class ArmPID {
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class ArmPID extends OpMode {
+public DcMotor slides = null;
+double TargetPos = 0;
+double CurrentPos = slides.getCurrentPosition();
+    @Override
+    public void init() {
+        slides = hardwareMap.get(DcMotor.class, "slides");
+        slides.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        slides.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        slides.setTargetPosition(0);
+        slides.setPower(0.5);
+        slides.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    }
+
+    @Override
+    public void loop() {
+        if (TargetPos > CurrentPos) {
+            slides.setPower(0.2);
+            boolean forward = true;
+        }else if (TargetPos < CurrentPos){
+            slides.setPower(-0.2);
+            boolean forward = false;
+        }
+
+    }
 }
