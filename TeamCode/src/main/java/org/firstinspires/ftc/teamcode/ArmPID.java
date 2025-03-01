3d63fdb798e1ab192f9797777567613d9450fbc8
oliver-error <oliverliamandersen2011@gmail.com>
2025-03-01 11:36:38 -0800

    basic PID
    this is when I created a basic P I and D

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmPID.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmPID.java
@@ -1,12 +1,16 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-
+@TeleOp
 public class ArmPID extends OpMode {
 public DcMotor slides = null;
-double TargetPos = 0;
-double CurrentPos = slides.getCurrentPosition();
+double TargetPos = 1000;
+double priorDistance = 0;
+double currentDistance = 0;
+boolean fastLoop = false;
+
     @Override
     public void init() {
         slides = hardwareMap.get(DcMotor.class, "slides");
@@ -19,13 +23,27 @@ double CurrentPos = slides.getCurrentPosition();
 
     @Override
     public void loop() {
-        if (TargetPos > CurrentPos) {
+        while (slides.getCurrentPosition() != TargetPos) { /* loops while current position is not equal to target position */
+            double CurrentPos = slides.getCurrentPosition();
+        if (TargetPos > CurrentPos) { //a rudimentary P in PID
             slides.setPower(0.2);
             boolean forward = true;
         }else if (TargetPos < CurrentPos){
             slides.setPower(-0.2);
             boolean forward = false;
         }
+            priorDistance = currentDistance; //a rudimentary I in PID
+            currentDistance = TargetPos - CurrentPos;
+
+        if (TargetPos - CurrentPos < 10) fastLoop = true; //almost a function that works like a D in PID
+
+        if (!fastLoop) {
+            aw
+        }
+
+            }
+        }
+      }
 
     }
 }
