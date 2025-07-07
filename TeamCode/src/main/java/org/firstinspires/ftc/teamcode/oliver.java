203e154c4aa81a918175d58f6dc4d3874bd78f73
oliver-error <oliverliamandersen2011@gmail.com>
2025-07-06 19:14:54 -0700

    added anti-cheat and to add difficulty scaling

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/oliver.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/oliver.java
@@ -5,6 +5,9 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 @TeleOp(name = "drive?")
 public class oliver extends OpMode {
     public Servo bugGoBrr = null;
@@ -18,7 +21,9 @@ public class oliver extends OpMode {
 
     @Override
     public void loop() {
-        if(gamepad1.a){
+        telemetry.addData("pressed?", gamepad1.a);
+        telemetry.update();
+        if(gamepad1.left_stick_button){
             bugGoBrr.setPosition(0.5);
         } else if(gamepad1.b){
             bugGoBrr.setPosition(1);
@@ -26,6 +31,21 @@ public class oliver extends OpMode {
             bugGoBrr.setPosition(0.0);
         } else if(gamepad1.x){
             bugGoBrr.setPosition(0.2);
+        } else if (gamepad1.a) {
+            while (gamepad1.a) {
+                try {
+                    Thread.sleep(100); // Sleep for 100 milliseconds
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt(); // avoids busy-waiting (why is it called that?)
+                }
+            }
+            motorGoBr.setPower(0.5);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            motorGoBr.setPower(0.0);
         }
 
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/oliverGame.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/oliverGame.java
@@ -5,11 +5,15 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@TeleOp(name = "oliverGame?")
+@TeleOp(name = "oliverGame")
 public class oliverGame extends OpMode {
     public Servo bugGoBrr = null;
     public DcMotor motorGoBr = null;
     public double servoPos = 0;
+    public int level = 1;
+    public int priorLevel = 1;
+
+
     @Override
     public void init() {
         bugGoBrr = hardwareMap.get(Servo.class, "bugGoBrr");
@@ -19,7 +23,9 @@ public class oliverGame extends OpMode {
 
     @Override
     public void loop() {
-        Thread thread1 = new Thread(() -> {
+        int servo = 0;
+        int motorI = 0;
+        Thread motor1 = new Thread(() -> {// triggers for rotation
             motorGoBr.setPower(0.1);
             try {
                 Thread.sleep(100);
@@ -27,34 +33,72 @@ public class oliverGame extends OpMode {
                 throw new RuntimeException(e);
             }
             motorGoBr.setPower(0.0);
+        });
+
+        Thread servo1 = new Thread(() -> {
+            servoPos = servoPos + 0.3333334;
+            bugGoBrr.setPosition(servoPos);
+        });
+
+        int run = (int) (Math.random() * (1000 - ((level * 100) - 99))); //random number generator (constantly looping)
+        if (run == 1) { // each has a varying chance of happening every loop based on the level
+            motor1.start(); //runs "trigger" (thread) motor1
+            motorI = motorI + 1;
+        } else if (run == 2) {
+            servo1.start();
+            servo = servo + 1;
+        } else if (run == 3) {
+
+        } else if (run == 4) {
+
+        }
+
+        if (servoPos >= 1) {
+            System.exit(1);
+        }
+
+        if (gamepad1.x) { //insures that the code at the bottom is run when you release the button after pressing it
+            while (gamepad1.x) {
+                try {
+                    Thread.sleep(100); // Sleep for 100 milliseconds
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt(); // avoids busy-waiting (why is it called that?)
+                }
+            }
+            motorGoBr.setPower(-0.1);
             try {
-                Thread.sleep(10000);
+                Thread.sleep(100);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
-        });
-
-        Thread thread2 = new Thread(() -> {
-            if (gamepad1.x){
-                motorGoBr.setPower(-0.1);
-            }
-        });
-
-        thread1.start();
-        thread2.start();
-
-         servoPos = servoPos + 0.00001;
-         bugGoBrr.setPosition(servoPos);
-         telemetry.addData("servoPos", String.valueOf(servoPos));
-         telemetry.update();
-         if(servoPos >= 1) {
-             System.exit(1);
-         }
-        if(gamepad1.a){
-            servoPos = servoPos-0.0006;
-            if(servoPos < 0) servoPos = 0;
+            motorGoBr.setPower(0.0);
+            motorI = motorI - 1;
         }
-
+        if (gamepad1.a) {
+            while (gamepad1.a) {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+            servoPos = servoPos - 0.3333334;
+            if (servoPos < 0) servoPos = 0;
+            bugGoBrr.setPosition(servoPos);
+            servo = servo - 1;
+        }
+        if ((servo >= 3) || (motorI >= 5)) {
+        System.exit(1);
+        }
+        if (level != priorLevel) { // level complete
+            while (!gamepad1.options) {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+            priorLevel = level;
+        }
     }
 }
-
