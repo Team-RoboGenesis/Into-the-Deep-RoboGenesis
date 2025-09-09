341755a199f197d73aeee4f16494bfe208eb3be3
oliver-error <oliverliamandersen2011@gmail.com>
2025-09-09 16:49:20 -0700

    added ?????
    ??????

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/oliverGame.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/oliverGame.java
@@ -12,6 +12,8 @@ public class oliverGame extends OpMode {
     public double servoPos = 0;
     public int level = 1;
     public int priorLevel = 1;
+    public boolean first = true;
+    public int caught = 0;
 
 
     @Override
@@ -23,93 +25,93 @@ public class oliverGame extends OpMode {
 
     @Override
     public void loop() {
-        int servo = 0;
-        int motorI = 0;
-        Thread motor1 = new Thread(() -> {// triggers for rotation
-            motorGoBr.setPower(0.1);
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            motorGoBr.setPower(0.0);
-        });
 
-        Thread servo1 = new Thread(() -> {
-            servoPos = servoPos + 0.3333334;
-            bugGoBrr.setPosition(servoPos);
-        });
-
-        int run = (int) (Math.random() * (1000 - ((level * 100) - 99))); //random number generator (constantly looping)
-        if (run == 1) { // each has a varying chance of happening every loop based on the level
-            motor1.start(); //runs "trigger" (thread) motor1
-            motorI = motorI + 1;
-        } else if (run == 2) {
-            servo1.start();
-            servo = servo + 1;
-        } else if (run == 3) {
-
-        } else if (run == 4) {
-
-        }
-
-        if (servoPos >= 1) {
-            System.exit(1);
-        }
-
-        if (gamepad1.x) { //insures that the code at the bottom is run when you release the button after pressing it
-            while (gamepad1.x) {
-                try {
-                    Thread.sleep(100); // Sleep for 100 milliseconds
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt(); // avoids busy-waiting (why is it called that?)
-                }
-            }
-            motorGoBr.setPower(-0.1);
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            motorGoBr.setPower(0.0);
-            motorI = motorI - 1;
-        }
-        if (gamepad1.a) {
-            while (gamepad1.a) {
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                }
-            }
-            servoPos = servoPos - 0.3333334;
-            if (servoPos < 0) servoPos = 0;
-            bugGoBrr.setPosition(servoPos);
-            servo = servo - 1;
-        }
-        if ((servo >= 3) || (motorI >= 5)) {
-        System.exit(1);
-        }
-        if (level != priorLevel) { // level complete
-            while (!gamepad1.options) {
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                }
-            }
-            priorLevel = level;
-        }
-        for(true) {
-            servoPos = 1;
+        if (first) {
+            first = false;
+            caught++;
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
+                throw new RuntimeException(e);
             }
-            servoPos = 0;
+            if(caught > 1) System.exit(1);
+            int servo = 0;
+            int motorI = 0;
+            Thread motor1 = new Thread(() -> {// triggers for rotation
+                motorGoBr.setPower(0.1);
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                motorGoBr.setPower(0.0);
+            });
+
+            Thread servo1 = new Thread(() -> {
+                servoPos = servoPos + 0.3333334;
+                bugGoBrr.setPosition(servoPos);
+            });
+
+            int run = (int) (Math.random() * (1000 - ((level * 100) - 99))); //random number generator (constantly looping)
+            if (run == 1) { // each has a varying chance of happening every loop based on the level
+                motor1.start(); //runs "trigger" (thread) motor1
+                motorI = motorI + 1;
+            } else if (run == 2) {
+                servo1.start();
+                servo = servo + 1;
+            } else if (run == 3) {
+
+            } else if (run == 4) {
+
+            }
+
+            if (servoPos >= 1) {
+                System.exit(1);
+            }
+
+            if (gamepad1.x) { //insures that the code at the bottom is run when you release the button after pressing it
+                while (gamepad1.x) {
+                    try {
+                        Thread.sleep(100); // Sleep for 100 milliseconds
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt(); // avoids busy-waiting (why is it called that?)
+                    }
+                }
+                motorGoBr.setPower(-0.1);
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                motorGoBr.setPower(0.0);
+                motorI = motorI - 1;
+            }
+            if (gamepad1.a) {
+                while (gamepad1.a) {
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                }
+                servoPos = servoPos - 0.3333334;
+                if (servoPos < 0) servoPos = 0;
+                bugGoBrr.setPosition(servoPos);
+                servo = servo - 1;
+            }
+            if ((servo >= 3) || (motorI >= 5)) {
+                System.exit(1);
+            }
+            if (level != priorLevel) { // level complete
+                while (!gamepad1.options) {
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                }
+                priorLevel = level;
+            }
         }
-
-
     }
 }
